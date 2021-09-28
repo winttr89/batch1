52a5558e542817294c8ee2079ae3dd0714c8f2d7
winttr89 <winttr89@gmail.com>
2021-09-28 12:47:58 +0000

    1

--- auto5.sh
+++ auto5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sub="111111"
+sub="e30983a5-2b64-42f5-a6be-1e0f0ad4d119"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch1/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch1/main/batch2.json
@@ -13,8 +13,8 @@ do
 	batch=$(( $batch + 1 ))
 	az batch account create --subscription "$sub" --name a$batch$nnn --resource-group batchacc$ran --location $region --no-wait
 done
-echo "Sleep 3m..."
-sleep 3m
+echo "Sleep 7m..."
+sleep 7m
 batch=0
 echo "Batch account setting..."
 for region in australiaeast canadacentral centralindia centralus eastus eastus2 francecentral germanywestcentral japaneast koreacentral northeurope southcentralus southeastasia switzerlandnorth uksouth westcentralus westeurope westus westus2 westus3
