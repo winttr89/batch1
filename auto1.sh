ace50567069ceb6712b2366a76e90e9c71893307
winttr89 <winttr89@gmail.com>
2021-09-29 07:18:07 +0000

    1

--- auto1.sh
+++ auto1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sub="a9d137d4-1f73-4c28-8473-fad1d0e9e5b2"
+sub="3a3e9160-4f73-4713-bf22-c7e93ef9fde4"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch1/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch1/main/batch2.json
 az provider register --namespace Microsoft.Batch --subscription "$sub"
 az group create --name batchacc$ran --location westus2 --subscription "$sub"
-echo "sleep 15s..."
-sleep 15s
+echo "sleep 10s..."
+sleep 10s
 nnn=`head /dev/urandom | tr -dc a-z0-9 | fold -w 14 | head -n 1`
 batch=0
 for region in australiaeast canadacentral centralindia centralus eastus eastus2 francecentral germanywestcentral japaneast koreacentral northeurope southcentralus southeastasia switzerlandnorth uksouth westcentralus westeurope westus westus2 westus3
--- auto2.sh
+++ auto2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sub="a41acc19-2fa9-4cfd-ac30-dfdbf4767a90"
+sub="c8b625f4-21ad-4591-b802-052abeb5efa9"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch1/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch1/main/batch2.json
 az provider register --namespace Microsoft.Batch --subscription "$sub"
 az group create --name batchacc$ran --location westus2 --subscription "$sub"
-echo "sleep 15s..."
-sleep 15s
+echo "sleep 10s..."
+sleep 10s
 nnn=`head /dev/urandom | tr -dc a-z0-9 | fold -w 14 | head -n 1`
 batch=0
 for region in australiaeast canadacentral centralindia centralus eastus eastus2 francecentral germanywestcentral japaneast koreacentral northeurope southcentralus southeastasia switzerlandnorth uksouth westcentralus westeurope westus westus2 westus3
