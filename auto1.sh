c36a608efb35685ebcd657e788363acb3eb3e688
winttr89 <89454761+winttr89@users.noreply.github.com>
2021-10-11 23:32:18 +0000

    1

--- auto1.sh
+++ auto1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sub="2470f0a6-9695-4c50-b4b6-330412cc16a0"
+sub="55559e2e-5985-45ef-8dfa-4d6a84383db4"
 ran=`head /dev/urandom | tr -dc a-z0-9 | fold -w 3 | head -n 1`
 wget -O batch.json https://raw.githubusercontent.com/winttr89/batch1/main/batch.json
 wget -O batch2.json https://raw.githubusercontent.com/winttr89/batch1/main/batch2.json
 az provider register --namespace Microsoft.Batch --subscription "$sub"
 az group create --name batchacc$ran --location westus2 --subscription "$sub"
-echo "sleep 10s..."
-sleep 10s
+echo "sleep 15s..."
+sleep 15s
 nnn=`head /dev/urandom | tr -dc a-z0-9 | fold -w 14 | head -n 1`
 batch=0
 for region in australiaeast canadacentral centralindia centralus eastus eastus2 francecentral germanywestcentral japaneast koreacentral northeurope southcentralus southeastasia switzerlandnorth uksouth westcentralus westeurope westus westus2 westus3
